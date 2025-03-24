2b4158f2964f966be203d91e2d328b46bba6c8d7
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-24 12:33:20 +0200

    Update server.js

--- chapter1/server.js
+++ chapter1/server.js
@@ -13,7 +13,8 @@ app.get('/users', async(req, res) => {
 // { id: 2, name: 'Bob Williams' },
 // { id: 3, name: 'Shannon Jackson'},
 // ]
-const response = await fetch('https://jsonplaceholder.typicode.com/users')
+const limit = +req.query.limit || 10;
+const response = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=${limit}`)
 const users = await response.json()
 res.send(`
 <h2>Users</h2>
