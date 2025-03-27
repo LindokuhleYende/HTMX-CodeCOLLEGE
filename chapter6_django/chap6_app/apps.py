97497fc323af5d1b639caac24443a0afe86119ed
Lindokuhle <yendelindokuhle2@gmail.com>
2025-03-27 11:07:15 +0200

    chapter6_django

---
+++ chapter6_django/chap6_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Chap6AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'chap6_app'
--- chapter6_django/chap6_project/settings.py
+++ chapter6_django/chap6_project/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "chap6_app",
+    "chap6_project",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
